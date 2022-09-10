97aa447716a3a50cdbc76a1859516694220d4614
ErikBentancor <erikbentancor@gmail.com>
2022-09-09 21:21:05 -0300

    actualizacion superusuario

--- appUsuario/admin.py
+++ appUsuario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from appUsuario.models import Usuario, Newsletter, Articulo
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Newsletter)
+admin.site.register(Articulo)
