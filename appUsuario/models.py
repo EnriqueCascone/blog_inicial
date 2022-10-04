9be90021efa1bca15718575c079ee596abb786d4
Erik Bentancor <erikbentancor@gmail.com>
2022-10-04 20:28:56 -0300

    agregado icono de avatar

--- appUsuario/models.py
+++ appUsuario/models.py
@@ -26,8 +26,9 @@ class Newsletter(models.Model):
 
 
 class Avatar(models.Model):
-    user = models.OneToOneField(Usuario, on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     imagen = models.ImageField(upload_to='avatares', null=True, blank = True)
 
     def __str__(self):
         return f"Imagen de: {self.user}"    
+        
