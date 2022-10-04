7865ff6916ffa1c4c5f403388a805c03f3b60809
Erik Bentancor <erikbentancor@gmail.com>
2022-10-04 03:02:21 -0300

    Agregador Login, Logout, Register. Agregado boton al inicio para ver un articulo. Login VInculado para cuando intenten entrar a una seccion que requiera login se redirigido automaticamente

--- appUsuario/urls.py
+++ appUsuario/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from appUsuario import views
 
+
+
 urlpatterns = [
 path('', views.inicio, name="inicio"),
 path('crear_usuario/', views.crear_usuario, name="crear_usuario"),
@@ -12,4 +14,8 @@ path('busquedaUsuario/', views.busquedaUsuario, name="busquedaUsuario"),
 path('buscar_usuario/', views.buscar_usuario, name="buscar_usuario"),
 path('eliminar_articulo/<int:id>', views.eliminarArticulo, name="eliminar_articulo"),
 path('editar_articulo/<int:id>', views.editar_articulo, name="editar_articulo"),
+path('ver_articulo/<int:id>', views.ver_articulo, name="ver_articulo"),
+path('login/', views.login_request, name="Login"),
+path('register/', views.register, name="Registro"),
+path('logout/', views.CustomLogoutView.as_view(), name="Registro"),
 ]
